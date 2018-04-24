619f2d0e16676f245077ba333a87df31cba89830
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-04-23 22:00:31 -0700

    Widget working now.

--- app/src/main/java/com/example/sumitasharma/easyvocabulary/wordwidget/EasyVocabularyWidget.java
+++ app/src/main/java/com/example/sumitasharma/easyvocabulary/wordwidget/EasyVocabularyWidget.java
@@ -5,62 +5,21 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.preference.PreferenceManager;
 import android.widget.RemoteViews;
-import android.widget.TextView;
 
 import com.example.sumitasharma.easyvocabulary.R;
+import com.example.sumitasharma.easyvocabulary.data.WordContract;
 import com.example.sumitasharma.easyvocabulary.wordui.MainActivity;
+
+import timber.log.Timber;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class EasyVocabularyWidget extends AppWidgetProvider {
-    TextView mWord;
-    TextView mMeaning;
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, String mWord, String mMeaning,
-                                int appWidgetId) {
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.easy_vocabulary_widget);
-
-        views.setTextViewText(R.id.wordwidget_text, mWord.substring(0, 1).toUpperCase() + mWord.substring(1).toLowerCase());
-        views.setTextViewText(R.id.meaningwidget_text, mMeaning.substring(0, 1).toUpperCase() + mMeaning.substring(1).toLowerCase());
-
-        //Create and Launch MainActivity when clicked
-        Intent intent = new Intent(context, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-        //Click Handler on Widget to launch Pending Intent
-        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
-        views.setOnClickPendingIntent(R.id.wordwidget_text, pendingIntent);
-        views.setOnClickPendingIntent(R.id.meaningwidget_text, pendingIntent);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
-    static void updateWordAppWidget(Context context, AppWidgetManager appWidgetManager, String mWord, String mMeaning,
-                                    int[] appWidgetIds) {
-
-
-        Log.i("BakingAppWidgetProvider", "Inside updateBakingAppWidget");
-
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, mWord, mMeaning, appWidgetId);
-        }
-
-
-    }
-
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        EasyVocabularyService.startActionUpdateWordApp(context);
-
-
-    }
 
     @Override
     public void onEnabled(Context context) {
@@ -71,5 +30,49 @@ public class EasyVocabularyWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        int numberOfWordsForPractice = Integer.parseInt(sharedPreferences.getString(context.getResources().getString(R.string.number_of_words_key), "1"));
+        String frequencyOfWordsForPractice = (sharedPreferences.getString(context.getResources().getString(R.string.frequency_of_words_key), "sharedpreference"));
+        String levelOfWordsForPractice = (sharedPreferences.getString(context.getResources().getString(R.string.level_of_words_for_practice_key), "shared"));
+        String[] projection = {WordContract.WordsEntry.COLUMN_WORD, WordContract.WordsEntry.COLUMN_WORD_MEANING};
+        String sort_order = "RANDOM() LIMIT 1";
+        Cursor retCursor = context.getContentResolver().query(WordContract.WordsEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                sort_order);
+        Timber.i("Inside query of content provider in widget" + retCursor.getCount());
+        if (retCursor.moveToFirst() && retCursor.getCount() >= 1) {
+
+            Timber.i("word in widget : " + retCursor.getString(0));
+            Timber.i("word meaning in widget : " + retCursor.getString(1));
+            final int N = appWidgetIds.length;
+
+            // Perform this loop procedure for each App Widget that belongs to this provider
+            for (int i = 0; i < N; i++) {
+                int appWidgetId = appWidgetIds[i];
+
+                // Create an Intent to launch ExampleActivity
+                Intent intent = new Intent(context, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+
+                RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.easy_vocabulary_widget);
+                views.setOnClickPendingIntent(R.id.meaningwidget_text, pendingIntent);
+                views.setOnClickPendingIntent(R.id.wordwidget_text, pendingIntent);
+                views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+                views.setOnClickPendingIntent(R.id.appwidget_hint_text, pendingIntent);
+                views.setTextViewText(R.id.wordwidget_text, retCursor.getString(0).substring(0, 1).toUpperCase() + retCursor.getString(0).substring(1).toLowerCase());
+                views.setTextViewText(R.id.meaningwidget_text, retCursor.getString(1).substring(0, 1).toUpperCase() + retCursor.getString(1).substring(1).toLowerCase());
+
+                // Tell the AppWidgetManager to perform an update on the current app widget
+                appWidgetManager.updateAppWidget(appWidgetId, views);
+            }
+        }
+    }
 }
+
 
