205f06292e1911494965ecae329943c90f17a09a
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-07-08 11:51:18 -0700

    Cleaned up Redundant comments

--- app/src/main/java/com/example/sumitasharma/easyvocabulary/loaders/LoadingDataFromCloud.java
+++ app/src/main/java/com/example/sumitasharma/easyvocabulary/loaders/LoadingDataFromCloud.java
@@ -41,55 +41,6 @@ public class LoadingDataFromCloud extends AsyncTaskLoader {
         forceLoad();
     }
 
-//    @Nullable
-//    @Override
-//    public Object loadInBackground() {
-//        Timber.i("Inside loadInBackground");
-//
-//        WordsDbUtil wordsDbUtil = new WordsDbUtil(mContext);
-//
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-//                .setTimestampsInSnapshotsEnabled(true)
-//                .build();
-//        db.setFirestoreSettings(settings);
-//
-//
-//        mTimingsLogger.addSplit("Total Time for All documents");
-//
-//        db.collection("dictionary")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                Timber.i(document.getId() + " => " + document.getData().get("word"));
-//                                // Create new empty ContentValues object
-//                                ContentValues contentValues = new ContentValues();
-//
-//                                // Put the task description and selected mPriority into the ContentValues
-//                                contentValues.put(WordContract.WordsEntry.COLUMN_WORD, (String) document.getData().get("word"));
-//                                contentValues.put(WordContract.WordsEntry.COLUMN_WORD_MEANING, (String) document.getData().get("wordMeaning"));
-//                                contentValues.put(WordContract.WordsEntry.COLUMN_WORD_LEVEL, (String) document.getData().get("wordLevel"));
-//                                contentValues.put(WordContract.WordsEntry.COLUMN_WORD_PRACTICED, false);
-//                                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-//                                String date = dateFormat.format(new Date());
-//                                contentValues.put(WordContract.WordsEntry.COLUMN_LAST_UPDATED, date);
-//                                // Insert the content values via a ContentResolver
-//                                mContext.getContentResolver().insert(WordContract.WordsEntry.CONTENT_URI, contentValues);
-//                                mTimingsLogger.addSplit("Single Document");
-//
-//                            }
-//                        } else {
-//                            Timber.i("Error getting documents.", task.getException());
-//                        }
-//                    }
-//                });
-//        mTimingsLogger.dumpToLog();
-//        return null;
-//    }
-//}
 
     @Nullable
     @Override
@@ -107,7 +58,7 @@ public class LoadingDataFromCloud extends AsyncTaskLoader {
 
         // mTimingsLogger.addSplit("Total Time for All documents");
         Timber.i("Loader: Setting up Query at: " + Calendar.getInstance().getTime());
-        Query limitWords = db.collection("dictionary").limit(100 0);
+        Query limitWords = db.collection("dictionary").limit(100);
 
 
         limitWords.get()
